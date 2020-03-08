c95f3d1891522c302a30556a5ba7426e5146fb42
zzBoss <1376351083@qq.com>
2020-03-08 12:41:14 +0800

    'init'

---
+++ block/qhMixlyExtension.js
@@ -0,0 +1,367 @@
+"use strict";
+goog.provide("Blockly.Blocks.QHMixlyExtension");
+goog.require("Blockly.Blocks");
+
+Blockly.qhColour = {};
+/**********************************************
+ * utils
+ *********************************************/
+/**
+ * 生成高低电平的下拉项
+ */
+function createHightLowDropdwon() {
+  const fieldDropdown = [
+    [Blockly.MIXLY_ON, "HIGH"],
+    [Blockly.MIXLY_OFF, "LOW"]
+  ];
+  return createDropDown(fieldDropdown);
+}
+
+/**
+ * 创建下拉项
+ * @param {Array} ItemArr 下拉项 [[item1,val1],[item2,val2]]
+ */
+function createDropDown(ItemArr) {
+  return new Blockly.FieldDropdown(ItemArr);
+}
+
+/**
+ * 块定义函数
+ * @param {String} blockName 块名称 
+ * @param {Function} initFunc 块初始化函数
+ */
+function defineBlock(blockName, initFunc) {
+  Blockly.Blocks[blockName] = {
+    init: initFunc,
+    setQHValue: function(key, val) {
+      this[key] = val;
+    },
+    getQhValue: function(key) {
+      return this[key];
+    },
+    setDefault: function(tooltip) {
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setInputsInline(true);
+      this.setTooltip(tooltip);
+    }
+  }
+}
+
+/********************************************
+ * 灯光模块
+ *********************************************/
+
+// 运行指示灯
+defineBlock('qh_indicator_light', function() {
+  this.setColour(Blockly.Blocks.base.HUE);
+  this.appendDummyInput("")
+    .appendField(Blockly.QH_INDICATOR_LIGHT_SWITCH)
+    .appendField(createHightLowDropdwon(), "MODE");
+  this.setDefault(Blockly.QH_TOOLTIP_INDICSTOR_LIGHT);
+});
+
+// RGB 灯控制
+defineBlock('qh_rgb_light', function() {
+  this.setColour(Blockly.Blocks.base.HUE);
+  this.appendDummyInput('')
+    .appendField(Blockly.QH_RGB_LIGHT_CHOOSE)
+    .appendField(Blockly.QH_RGB_LIGHT_R)
+    .appendField(createHightLowDropdwon(), 'R')
+    .appendField(Blockly.QH_RGB_LIGHT_G)
+    .appendField(createHightLowDropdwon(), 'G')
+    .appendField(Blockly.QH_RGB_LIGHT_B)
+    .appendField(createHightLowDropdwon(), 'B')
+  this.setDefault(Blockly.QH_TOOLTIP_RGB_LIGHT);
+});
+
+// 随机rgb灯光
+defineBlock('qh_random_rgb', function() {
+  this.setColour(Blockly.Blocks.base.HUE);
+  this.appendDummyInput('')
+    .appendField(Blockly.QH_RGB_RANDOM)
+  this.setDefault(Blockly.QH_TOOLTIP_RGB_RANDOM);
+});
+
+/********************************************
+ * 模块
+ *********************************************/
+Blockly.qhColour.MODEL = '#2E8B57';
+// 超声波测距
+defineBlock('qh_ultrasonic_ranging', function() {
+  this.setColour(Blockly.qhColour.MODEL);
+  this.appendDummyInput('')
+    .appendField(Blockly.QH_ULTRASONIC_RANGING);
+  this.setOutput(true, Number);
+  this.setInputsInline(true);
+  this.setTooltip(Blockly.QH_TOOLTIP_ULTRASONIC_RANGING);
+});
+
+// 舵机转动
+defineBlock('qh_servo_angle', function() {
+  this.setColour(Blockly.qhColour.MODEL);
+  this.appendValueInput('angle')
+    .setCheck(Number)
+    .appendField(Blockly.QH_SERVO_ANGLE);
+  this.appendDummyInput('')
+    .appendField(Blockly.QH_SERVO_ANGLE_UNIT);
+  this.setDefault(Blockly.QH_TOOLTIP_SERVO_ANGLE);
+});
+
+/********************************************
+ * 运动模块
+ *********************************************/
+Blockly.qhColour.MOTION = '#FFB6C1';
+// 小车运动  0:刹车 1:前进 2:后退 3:左转 4:右转 5:原地左转 6:原地右转
+defineBlock('qh_car_base_motion', function() {
+  const selection = [
+    [Blockly.QH_FORWARD, '1'],
+    [Blockly.QH_BACKWARD, '2'],
+    [Blockly.QH_TURN_LEFT, '3'],
+    [Blockly.QH_TURN_RIGHT, '4'],
+    [Blockly.QH_SPIN_RIGHT, '5'],
+    [Blockly.QH_SPIN_RIGHT, '6']
+  ];
+  this.setColour(Blockly.qhColour.MOTION);
+  this.appendValueInput('power')
+    .setCheck(Number)
+    .appendField(Blockly.QH_CAR_DIRECTION)
+    .appendField(createDropDown(selection), 'direction')
+    .appendField(Blockly.QH_VELOCITY);
+  this.setDefault(Blockly.QH_TOOLTIP_CAR_MOTION);
+});
+
+// 定义不同方向的小车运动块
+function defineCarControlBlock(blockName, dirVal, desc) {
+  defineBlock(blockName, function() {
+    this.setColour(Blockly.qhColour.MOTION);
+    this.appendValueInput('power')
+      .setCheck(Number)
+      .appendField(desc);
+    this.appendValueInput('secs')
+      .setCheck(Number)
+      .appendField(Blockly.QH_PERCENT)
+      .appendField(Blockly.QH_MOVE);
+    this.appendDummyInput('')
+      .appendField(Blockly.QH_SECONDS);
+    this.setDefault(Blockly.QH_TOOLTIP_CAR_CONTROL);
+    this.setQHValue('direction', dirVal);
+  });
+}
+
+defineCarControlBlock('qh_car_farword', '1', Blockly.QH_FARWORD_WITH_POWER);
+defineCarControlBlock('qh_car_backword', '2', Blockly.QH_BACKWORD_WITH_POWER);
+defineCarControlBlock('qh_car_turn_left', '3', Blockly.QH_TURN_LEFT_WITH_POWER);
+defineCarControlBlock('qh_car_turn_right', '4', Blockly.QH_TURN_RIGHT_WITH_POWER);
+defineCarControlBlock('qh_car_spin_left', '5', Blockly.QH_SPIN_LEFT_WITH_POWER);
+defineCarControlBlock('qh_car_spin_right', '6', Blockly.QH_SPIN_RIGHT_WITH_POWER);
+
+// 小车停止
+defineBlock('qh_car_pause', function() {
+  this.setColour(Blockly.qhColour.MOTION);
+  this.appendDummyInput('')
+    .appendField(Blockly.QH_CAR_PAUSE);
+  this.setDefault(Blockly.QH_CAR_PAUSE);
+});
+
+/********************************************
+遥控 - 红外遥控 、 PS2-手柄遥控
+*********************************************/
+Blockly.qhColour.PS2 = '#47C5CA';
+//红外接收模块
+Blockly.Blocks.yf_ir_recv = {
+  init: function() {
+    this.setColour(Blockly.qhColour.PS2);
+    this.appendDummyInput()
+      .appendField(Blockly.MIXLY_YF_IR_RECEIVE)
+      .appendField(new Blockly.FieldImage("../../media/yfrobot/yf_irR.png", 44, 32))
+      .appendField(" ")
+      .appendField(new Blockly.FieldTextInput('ir_item'), 'VAR');
+    this.appendValueInput("PIN", Number)
+      .appendField(Blockly.MIXLY_PIN)
+      .setCheck(Number);
+    this.appendStatementInput('DO')
+      .appendField(Blockly.MIXLY_IR_RECEIVE_YES);
+    this.appendStatementInput('DO2')
+      .appendField(Blockly.MIXLY_IR_RECEIVE_NO);
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setInputsInline(true);
+    this.setTooltip(Blockly.MIXLY_IR_RECIEVE_TOOLTIP);
+  },
+  getVars: function() {
+    return [this.getFieldValue('VAR')];
+  },
+  renameVar: function(oldName, newName) {
+    if (Blockly.Names.equals(oldName, this.getFieldValue('VAR'))) {
+      this.setTitleValue(newName, 'VAR');
+    }
+  }
+};
+
+//红外mini遥控器键值
+var YF_IR_VAL = [
+  [{ 'src': '../../media/yfrobot/yf_ir_val/yf_ir_pow.png', 'width': 32, 'height': 32 }, '0xFFA25D'],
+  [{ 'src': '../../media/yfrobot/yf_ir_val/yf_ir_menu.png', 'width': 32, 'height': 32 }, '0xFFE21D'],
+  [{ 'src': '../../media/yfrobot/yf_ir_val/yf_ir_test.png', 'width': 32, 'height': 32 }, '0xFF22DD'],
+  [{ 'src': '../../media/yfrobot/yf_ir_val/yf_ir_+.png', 'width': 32, 'height': 32 }, '0xFF02FD'],
+  [{ 'src': '../../media/yfrobot/yf_ir_val/yf_ir_back.png', 'width': 32, 'height': 32 }, '0xFFC23D'],
+  [{ 'src': '../../media/yfrobot/yf_ir_val/yf_ir_rewind.png', 'width': 32, 'height': 32 }, '0xFFE01F'],
+  [{ 'src': '../../media/yfrobot/yf_ir_val/yf_ir_play.png', 'width': 32, 'height': 32 }, '0xFFA857'],
+  [{ 'src': '../../media/yfrobot/yf_ir_val/yf_ir_fastForward.png', 'width': 32, 'height': 32 }, '0xFF906F'],
+  [{ 'src': '../../media/yfrobot/yf_ir_val/yf_ir_0.png', 'width': 32, 'height': 32 }, '0xFF6897'],
+  [{ 'src': '../../media/yfrobot/yf_ir_val/yf_ir_-.png', 'width': 32, 'height': 32 }, '0xFF9867'],
+  [{ 'src': '../../media/yfrobot/yf_ir_val/yf_ir_c.png', 'width': 32, 'height': 32 }, '0xFFB04F'],
+  [{ 'src': '../../media/yfrobot/yf_ir_val/yf_ir_1.png', 'width': 32, 'height': 32 }, '0xFF30CF'],
+  [{ 'src': '../../media/yfrobot/yf_ir_val/yf_ir_2.png', 'width': 32, 'height': 32 }, '0xFF18E7'],
+  [{ 'src': '../../media/yfrobot/yf_ir_val/yf_ir_3.png', 'width': 32, 'height': 32 }, '0xFF7A85'],
+  [{ 'src': '../../media/yfrobot/yf_ir_val/yf_ir_4.png', 'width': 32, 'height': 32 }, '0xFF10EF'],
+  [{ 'src': '../../media/yfrobot/yf_ir_val/yf_ir_5.png', 'width': 32, 'height': 32 }, '0xFF38C7'],
+  [{ 'src': '../../media/yfrobot/yf_ir_val/yf_ir_6.png', 'width': 32, 'height': 32 }, '0xFF5AA5'],
+  [{ 'src': '../../media/yfrobot/yf_ir_val/yf_ir_7.png', 'width': 32, 'height': 32 }, '0xFF42BD'],
+  [{ 'src': '../../media/yfrobot/yf_ir_val/yf_ir_8.png', 'width': 32, 'height': 32 }, '0xFF4AB5'],
+  [{ 'src': '../../media/yfrobot/yf_ir_val/yf_ir_9.png', 'width': 32, 'height': 32 }, '0xFF52AD'],
+];
+Blockly.Blocks.yf_ir_val = {
+  init: function() {
+    this.setColour(Blockly.qhColour.PS2);
+    this.appendDummyInput()
+      .appendField(Blockly.MIXLY_YF_IR_VAL)
+      .appendField(new Blockly.FieldDropdown(YF_IR_VAL), 'VAL');
+    this.setOutput(true, Number);
+    this.setTooltip(Blockly.MIXLY_IR_RECIEVE_TOOLTIP);
+  }
+};
+
+// PS2 Controller init
+Blockly.Blocks.yf_ps2_init = {
+  init: function() {
+    this.setColour(Blockly.qhColour.PS2);
+    this.appendDummyInput("")
+      .appendField(Blockly.MIXLY_YF_PS2)
+      .appendField(new Blockly.FieldImage("../../media/qheduino/yf_ps2x.png", 60, 37))
+      .appendField(Blockly.MIXLY_SETUP)
+    this.appendValueInput("PIN1")
+      .setCheck(Number).setAlign(Blockly.ALIGN_RIGHT)
+      .appendField("DAT")
+    this.appendValueInput("PIN2")
+      .setCheck(Number).setAlign(Blockly.ALIGN_RIGHT)
+      .appendField("CMD")
+    this.appendValueInput("PIN3")
+      .setCheck(Number).setAlign(Blockly.ALIGN_RIGHT)
+      .appendField("CS")
+    this.appendValueInput("PIN4")
+      .setCheck(Number).setAlign(Blockly.ALIGN_RIGHT)
+      .appendField("CLK")
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+  }
+};
+
+//PS2手柄键值 - button 数字
+var MIXLY_YF_PS2_BTN = [
+  [{ 'src': '../../media/qheduino/yf_ps2_val/yf_ps2_start.png', 'width': 32, 'height': 20 }, 'PSB_START'],
+  [{ 'src': '../../media/qheduino/yf_ps2_val/yf_ps2_select.png', 'width': 32, 'height': 21 }, 'PSB_SELECT'],
+  [{ 'src': '../../media/qheduino/yf_ps2_val/yf_ps2_up.png', 'width': 52, 'height': 52 }, 'PSB_PAD_UP'],
+  [{ 'src': '../../media/qheduino/yf_ps2_val/yf_ps2_right.png', 'width': 52, 'height': 52 }, 'PSB_PAD_RIGHT'],
+  [{ 'src': '../../media/qheduino/yf_ps2_val/yf_ps2_left.png', 'width': 52, 'height': 52 }, 'PSB_PAD_LEFT'],
+  [{ 'src': '../../media/qheduino/yf_ps2_val/yf_ps2_down.png', 'width': 52, 'height': 52 }, 'PSB_PAD_DOWN'],
+  [{ 'src': '../../media/qheduino/yf_ps2_val/yf_ps2_cross.png', 'width': 32, 'height': 32 }, 'PSB_CROSS'],
+  [{ 'src': '../../media/qheduino/yf_ps2_val/yf_ps2_circle.png', 'width': 32, 'height': 32 }, 'PSB_CIRCLE'],
+  [{ 'src': '../../media/qheduino/yf_ps2_val/yf_ps2_square.png', 'width': 32, 'height': 32 }, 'PSB_SQUARE'],
+  [{ 'src': '../../media/qheduino/yf_ps2_val/yf_ps2_triangle.png', 'width': 32, 'height': 32 }, 'PSB_TRIANGLE'],
+  [{ 'src': '../../media/qheduino/yf_ps2_val/yf_ps2_l1.png', 'width': 73, 'height': 32 }, 'PSB_L1'],
+  [{ 'src': '../../media/qheduino/yf_ps2_val/yf_ps2_r1.png', 'width': 73, 'height': 32 }, 'PSB_R1'],
+  [{ 'src': '../../media/qheduino/yf_ps2_val/yf_ps2_l2.png', 'width': 73, 'height': 37 }, 'PSB_L2'],
+  [{ 'src': '../../media/qheduino/yf_ps2_val/yf_ps2_r2.png', 'width': 73, 'height': 37 }, 'PSB_R2'],
+  [{ 'src': '../../media/qheduino/yf_ps2_val/yf_ps2_l3.png', 'width': 48, 'height': 48 }, 'PSB_L3'],
+  [{ 'src': '../../media/qheduino/yf_ps2_val/yf_ps2_r3.png', 'width': 48, 'height': 48 }, 'PSB_R3'],
+];
+
+const MIXLY_YF_PS2_BTN_STA = [
+  [Blockly.MIXLY_YF_PSB_PRESSED, "ButtonPressed"],
+  [Blockly.MIXLY_YF_PSB_RELEASED, "ButtonReleased"]
+];
+
+// PS2 Controller button 
+defineBlock('yf_ps2_btn', function() {
+  this.setColour(Blockly.qhColour.PS2);
+  this.appendDummyInput("")
+    .appendField(Blockly.MIXLY_YF_PS2_BUTTON)
+    .appendField(new Blockly.FieldImage("../../media/qheduino/yf_ps2x.png", 60, 37))
+    .appendField(new Blockly.FieldDropdown(MIXLY_YF_PS2_BTN), "PS2_BTN")
+    .appendField(Blockly.MIXLY_YF_PS2_BTN_STATUS)
+    .appendField(new Blockly.FieldDropdown(MIXLY_YF_PS2_BTN_STA), "PS2_BTN_STATUS");
+  this.setOutput(true, Number);
+  this.setInputsInline(true);
+  this.setTooltip('');
+});
+
+//PS2手柄摇杆
+const MIXLY_YF_PS2_ROCKER_STA = [
+  [{ 'src': '../../media/qheduino/yf_ps2_val/yf_ps2_lx.png', 'width': 48, 'height': 48 }, 'PSS_LX'],
+  [{ 'src': '../../media/qheduino/yf_ps2_val/yf_ps2_ly.png', 'width': 48, 'height': 48 }, 'PSS_LY'],
+  [{ 'src': '../../media/qheduino/yf_ps2_val/yf_ps2_rx.png', 'width': 48, 'height': 48 }, 'PSS_RX'],
+  [{ 'src': '../../media/qheduino/yf_ps2_val/yf_ps2_ry.png', 'width': 48, 'height': 48 }, 'PSS_RY'],
+];
+
+// PS2 Controller Rocker
+defineBlock('yf_ps2_rocker', function() {
+  this.setColour(Blockly.qhColour.PS2);
+  this.appendDummyInput("")
+    .appendField(Blockly.MIXLY_YF_PS2_ROCKER)
+    .appendField(new Blockly.FieldImage("../../media/qheduino/yf_ps2x.png", 60, 37))
+    .appendField(new Blockly.FieldDropdown(MIXLY_YF_PS2_ROCKER_STA), "PS2_ROCKER_STATUS");
+  this.setOutput(true, Number);
+  this.setInputsInline(true);
+  this.setTooltip('');
+});
+
+//PS2手柄键值 - A button 模拟
+const MIXLY_YF_PS2_A_BTN = [
+  [{ 'src': '../../media/qheduino/yf_ps2_val/yf_ps2_up.png', 'width': 52, 'height': 52 }, 'PSAB_PAD_UP'],
+  [{ 'src': '../../media/qheduino/yf_ps2_val/yf_ps2_right.png', 'width': 52, 'height': 52 }, 'PSAB_PAD_RIGHT'],
+  [{ 'src': '../../media/qheduino/yf_ps2_val/yf_ps2_left.png', 'width': 52, 'height': 52 }, 'PSAB_PAD_LEFT'],
+  [{ 'src': '../../media/qheduino/yf_ps2_val/yf_ps2_down.png', 'width': 52, 'height': 52 }, 'PSAB_PAD_DOWN'],
+  [{ 'src': '../../media/qheduino/yf_ps2_val/yf_ps2_cross.png', 'width': 32, 'height': 32 }, 'PSAB_CROSS'],
+  [{ 'src': '../../media/qheduino/yf_ps2_val/yf_ps2_circle.png', 'width': 32, 'height': 32 }, 'PSAB_CIRCLE'],
+  [{ 'src': '../../media/qheduino/yf_ps2_val/yf_ps2_square.png', 'width': 32, 'height': 32 }, 'PSAB_SQUARE'],
+  [{ 'src': '../../media/qheduino/yf_ps2_val/yf_ps2_triangle.png', 'width': 32, 'height': 32 }, 'PSAB_TRIANGLE'],
+  [{ 'src': '../../media/qheduino/yf_ps2_val/yf_ps2_l1.png', 'width': 73, 'height': 32 }, 'PSB_L1'],
+  [{ 'src': '../../media/qheduino/yf_ps2_val/yf_ps2_r1.png', 'width': 73, 'height': 32 }, 'PSB_R1'],
+  [{ 'src': '../../media/qheduino/yf_ps2_val/yf_ps2_l2.png', 'width': 73, 'height': 37 }, 'PSB_L2'],
+  [{ 'src': '../../media/qheduino/yf_ps2_val/yf_ps2_r2.png', 'width': 73, 'height': 37 }, 'PSB_R2'],
+];
+
+// PS2 Controller read analog value of the button  --- how hard you press the button
+defineBlock('yf_ps2_a_btn', function() {
+  this.setColour(Blockly.qhColour.PS2);
+  this.appendDummyInput("")
+    .appendField(Blockly.MIXLY_YF_PS2_A_BUTTON)
+    .appendField(new Blockly.FieldImage("../../media/qheduino/yf_ps2x.png", 60, 37))
+    .appendField(new Blockly.FieldDropdown(MIXLY_YF_PS2_A_BTN), "PS2_A_BTN");
+  this.setOutput(true, Number);
+  this.setInputsInline(true);
+  this.setTooltip('');
+});
+
+// PS2 Controller read controller and setmotor
+defineBlock('yf_ps2_readController_setMotor', function() {
+  this.setColour(Blockly.qhColour.PS2);
+  this.appendDummyInput("")
+    .appendField(Blockly.MIXLY_YF_PS2)
+    .appendField(new Blockly.FieldImage("../../media/yfrobot/yf_ps2x.png", 60, 37))
+    .appendField(Blockly.MIXLY_YF_PS2_R_MOTOR)
+    .appendField("MOTOR1")
+    .appendField(new Blockly.FieldDropdown([
+      ["true", "true"],
+      ["false", "false"]
+    ]), "MOTOR1");
+  this.appendValueInput("MOTOR2")
+    .setCheck(Number).setAlign(Blockly.ALIGN_RIGHT)
+    .appendField("MOTOR2");
+  this.setInputsInline(true);
+  this.setPreviousStatement(true, null);
+  this.setNextStatement(true, null);
+});
---
+++ generator/qhMixlyExtension.js
@@ -0,0 +1,244 @@
+'use strict';
+goog.provide('Blockly.Arduino.QHMixlyExtend');
+goog.require('Blockly.Arduino');
+goog.require('Blockly.JavaScript');
+
+/*********************
+ * 常量定义
+ ********************/
+const OUTPUT = 'OUTPUT';
+const INPUT = 'INPUT';
+
+/*******************
+ * utils
+ *******************/
+/**
+ * 设置一组引脚
+ * @param {Array} pinModeArray 引脚定义的数组 ep:[[9,'HIGH'],[10,'LOW']] 
+ */
+function setPinModeArray(pinModeArray) {
+  pinModeArray.forEach(setPinMode);
+}
+
+/**
+ * 设置单个引脚
+ * @param {Array} pinMode 
+ */
+function setPinMode(pinMode) {
+  let pinName = 'setup_output_' + pinMode[0];
+  Blockly.Arduino.setups_[pinName] = 'pinMode(' + pinMode[0] + ', ' + pinMode[1] + ');';
+}
+
+/**
+ * 定义块的代码生成器
+ * @param {String} name 块名称
+ * @param {Function} func 生成器函数
+ */
+function defineBlockGenerator(name, func) {
+  Blockly.Arduino[name] = func;
+}
+
+/**
+ * 库引入代码生成
+ * @param {String} key 库定义所需键 
+ * @param {String} libName 引入的库名称
+ */
+function defineInclude(key, libName) {
+  Blockly.Arduino.definitions_[key] = '#include "' + libName + '"';
+}
+
+/**
+ * 引入沁和库文件
+ */
+function importQH() {
+  defineInclude('qh_include_qheduino', 'qheduino.h');
+}
+
+/**
+ * 变量定义代码生成
+ * @param {String} key 变量定义所需键 
+ * @param {*} code 变量定义的代码
+ */
+function defineVariable(key, code) {
+  Blockly.Arduino.definitions_[key] = code + ';';
+}
+/***************************
+ * 
+ * 基础模块
+ **************************/
+
+// 运行指示灯: 引脚-9
+defineBlockGenerator('qh_indicator_light', function() {
+  setPinMode([9, OUTPUT]);
+  var dropdown_mode = this.getFieldValue('MODE');
+  var code = 'pinMode(9, ' + dropdown_mode + ');\n';
+  return code;
+});
+
+// RGB 灯控制: r-10 g-9 b-11
+defineBlockGenerator('qh_rgb_light', function() {
+  setPinModeArray([
+    [10, OUTPUT],
+    [9, OUTPUT],
+    [11, OUTPUT]
+  ]);
+  let code = 'digitalWrite(10, ' + this.getFieldValue('R') + ');\n' +
+    'digitalWrite(9, ' + this.getFieldValue('G') + ');\n' +
+    'digitalWrite(11, ' + this.getFieldValue('B') + ');\n';
+  return code;
+});
+
+// 随机RGB灯光 : r-10 g-9 b-11
+defineBlockGenerator('qh_random_rgb', function() {
+  setPinModeArray([
+    [9, OUTPUT],
+    [10, OUTPUT],
+    [11, OUTPUT]
+  ]);
+  Blockly.Arduino.setups_['random_seed'] = 'randomSeed(analogRead(0));';
+
+  let code = 'digitalWrite(9, random(0,2));\n' +
+    'digitalWrite(10, random(0,2));\n' +
+    'digitalWrite(11, random(0,2));\n';
+  return code;
+});
+
+// 超声波测距
+defineBlockGenerator('qh_ultrasonic_ranging', function() {
+  defineInclude('qh_include_qheduino', 'qheduino.h');
+  defineVariable('qh_variable_SR04_13_12', 'SR04 SR04_13_12(13,12)');
+  let code = 'SR04_13_12.DistanceAvg()';
+  return [code, Blockly.Arduino.ORDER_ATOMIC];
+});
+
+// 舵机转动
+defineBlockGenerator('qh_servo_angle', function() {
+  let angle = Blockly.Arduino.valueToCode(this, 'angle', Blockly.Arduino.ORDER_ATOMIC) || 90;
+  defineInclude('qh_servo_angle', 'Servo.h');
+  defineVariable('qh_servo', 'Servo myservo');
+  Blockly.Arduino.setups_['qh_attach_pin'] = 'myservo.attach(3);';
+  let code = 'myservo.write(' + angle + ');\n';
+  return code;
+});
+
+// 小车运动控制
+defineBlockGenerator('qh_car_base_motion', function() {
+  defineInclude('qh_include_qheduino', 'qheduino.h');
+  let direction = this.getFieldValue('direction');
+  let velocity = Blockly.Arduino.valueToCode(this, 'power', Blockly.Arduino.ORDER_ATOMIC);
+  defineVariable('qh_car', 'CAR car(7,8,6,2,4,5)');
+  let code = 'car.direction_speed_ctrl(' + direction + ', ' + velocity + ');\n';
+  return code;
+});
+
+// 小车运动
+function defineCarControlBlockGenerator(name) {
+  defineBlockGenerator(name, function() {
+    importQH();
+    let direction = this.getQhValue('direction');
+    let power = Blockly.Arduino.valueToCode(this, 'power', Blockly.Arduino.ORDER_ATOMIC);
+    let secs = Blockly.Arduino.valueToCode(this, 'secs', Blockly.Arduino.ORDER_ATOMIC);
+    defineVariable('qh_car', 'CAR car(7,8,6,2,4,5)');
+    let code = 'car.direction_speed_ctrl(' + direction + ', ' + power + ');\n' +
+      'delay(' + (parseInt(secs) * 1000) + ');\n';
+    return code;
+  });
+}
+
+defineCarControlBlockGenerator('qh_car_farword');
+defineCarControlBlockGenerator('qh_car_backword');
+defineCarControlBlockGenerator('qh_car_turn_left');
+defineCarControlBlockGenerator('qh_car_turn_right');
+defineCarControlBlockGenerator('qh_car_spin_left');
+defineCarControlBlockGenerator('qh_car_spin_right');
+
+// 小车停止运动
+defineBlockGenerator('qh_car_pause', function() {
+  importQH();
+  defineVariable('qh_car', 'CAR car(7,8,6,2,4,5)');
+  return 'car.direction_speed_ctrl(0, 0);\n'
+});
+
+
+/********************************************
+遥控 - 红外遥控 、 PS2-手柄遥控
+*********************************************/
+//红外接收模块
+defineBlockGenerator('yf_ir_recv', function() {
+  var variable = Blockly.Arduino.variableDB_.getName(this.getFieldValue('VAR'), Blockly.Variables.NAME_TYPE);
+  Blockly.Arduino.definitions_['var_declare' + variable] = 'long ' + variable + ';';
+  var dropdown_pin = Blockly.Arduino.valueToCode(this, 'PIN', Blockly.Arduino.ORDER_ATOMIC);
+  var branch = Blockly.Arduino.statementToCode(this, 'DO');
+  var branch2 = Blockly.Arduino.statementToCode(this, 'DO2');
+  Blockly.Arduino.definitions_['define_ir_recv'] = '#include <IRremote.h>';
+  Blockly.Arduino.definitions_['var_ir_recv' + dropdown_pin] = 'IRrecv irrecv_' + dropdown_pin + '(' + dropdown_pin + ');\ndecode_results results_' + dropdown_pin + ';\n';
+  Blockly.Arduino.setups_['setup_ir_recv_' + dropdown_pin] = 'irrecv_' + dropdown_pin + '.enableIRIn();';
+  var code = "if (irrecv_" + dropdown_pin + ".decode(&results_" + dropdown_pin + ")) {\n"
+  code += '  ' + variable + '=results_' + dropdown_pin + '.value;\n';
+  code += '  String type="UNKNOWN";\n';
+  code += '  String typelist[14]={"UNKNOWN", "NEC", "SONY", "RC5", "RC6", "DISH", "SHARP", "PANASONIC", "JVC", "SANYO", "MITSUBISHI", "SAMSUNG", "LG", "WHYNTER"};\n';
+  code += '  if(results_' + dropdown_pin + '.decode_type>=1&&results_' + dropdown_pin + '.decode_type<=13){\n';
+  code += '    type=typelist[results_' + dropdown_pin + '.decode_type];\n'
+  code += '  }\n';
+  code += '  Serial.print("IR TYPE:"+type+"  ");\n';
+  code += branch;
+  code += '  irrecv_' + dropdown_pin + '.resume();\n'
+  code += '} else {\n';
+  code += branch2;
+  code += '}\n';
+  return code;
+});
+
+//红外mini遥控器键值
+defineBlockGenerator('yf_ir_val', function() {
+  var code = (this.getFieldValue('VAL'));
+  var order = code < 0 ?
+    Blockly.Arduino.ORDER_UNARY_PREFIX : Blockly.Arduino.ORDER_ATOMIC;
+  return [code, order];
+});
+
+// PS2 Controller init
+defineBlockGenerator('yf_ps2_init', function() {
+  var PS2_DAT = Blockly.Arduino.valueToCode(this, 'PIN1', Blockly.Arduino.ORDER_ATOMIC || '10');
+  var PS2_CMD = Blockly.Arduino.valueToCode(this, 'PIN2', Blockly.Arduino.ORDER_ATOMIC || '11');
+  var PS2_CS = Blockly.Arduino.valueToCode(this, 'PIN3', Blockly.Arduino.ORDER_ATOMIC || '12');
+  var PS2_CLK = Blockly.Arduino.valueToCode(this, 'PIN4', Blockly.Arduino.ORDER_ATOMIC || '13');
+  Blockly.Arduino.definitions_['include_ps2'] = '#include "PS2X_lib.h"';
+  Blockly.Arduino.definitions_['define_ps2_dat'] = '#define PS2_DAT ' + PS2_DAT;
+  Blockly.Arduino.definitions_['define_ps2_cmd'] = '#define PS2_CMD ' + PS2_CMD;
+  Blockly.Arduino.definitions_['define_ps2_cs'] = '#define PS2_CS ' + PS2_CS;
+  Blockly.Arduino.definitions_['define_ps2_clk'] = '#define PS2_CLK ' + PS2_CLK;
+  Blockly.Arduino.definitions_['var_ps2x'] = 'PS2X ps2x;';
+  Blockly.Arduino.setups_['setup_init_ps2'] = 'ps2x.config_gamepad(PS2_CLK, PS2_CMD, PS2_CS, PS2_DAT, true, true);\n';
+  return '';
+});
+
+// PS2 Controller button
+defineBlockGenerator('yf_ps2_btn', function() {
+  var PS2_BTN = Blockly.Arduino.variableDB_.getName(this.getFieldValue('PS2_BTN'), Blockly.Variables.NAME_TYPE);
+  var PS2_BTN_STATUS = Blockly.Arduino.variableDB_.getName(this.getFieldValue('PS2_BTN_STATUS'), Blockly.Variables.NAME_TYPE);
+  var code = 'ps2x.' + PS2_BTN_STATUS + '(' + PS2_BTN + ')';
+  return [code, Blockly.Arduino.ORDER_ATOMIC];
+});
+
+// PS2 Controller Rocker
+defineBlockGenerator('yf_ps2_rocker', function() {
+  var PS2_ROCKER_STATUS = Blockly.Arduino.variableDB_.getName(this.getFieldValue('PS2_ROCKER_STATUS'), Blockly.Variables.NAME_TYPE);
+  var code = 'ps2x.Analog(' + PS2_ROCKER_STATUS + ')';
+  return [code, Blockly.Arduino.ORDER_ATOMIC];
+});
+
+// PS2 Controller read analog value of the button  --- how hard you press the button
+defineBlockGenerator('yf_ps2_a_btn', function() {
+  var PS2_A_BTN = this.getFieldValue('PS2_A_BTN');
+  var code = 'ps2x.Analog(' + PS2_A_BTN + ')';
+  return [code, Blockly.Arduino.ORDER_ATOMIC];
+});
+
+// PS2 Controller read controller and setmotor
+defineBlockGenerator('yf_ps2_readController_setMotor', function() {
+  var PS2_Motor1 = this.getFieldValue('MOTOR1');
+  var PS2_Motor2 = Blockly.Arduino.valueToCode(this, 'MOTOR2', Blockly.Arduino.ORDER_ATOMIC || '0');
+  var code = 'ps2x.read_gamepad(' + PS2_Motor1 + ',' + PS2_Motor2 + ');\n'
+  return code;
+});
